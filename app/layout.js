1b3a06ddb3a92a10f9091bf26395e7ba81262efe
yonitfranko <yonitfranko@gmail.com>
2024-12-29 14:26:55 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,7 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'ניהול נקודות כיתתיות',
-  description: 'מערכת לניהול נקודות וצבירת פרסים',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="he" dir="rtl">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
