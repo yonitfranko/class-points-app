aea270d15ee500c2d45575235171feacb79847ab
yonitfranko <yonitfranko@gmail.com>
2024-12-29 14:34:58 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,10 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'ניהול נקודות כיתתיות',
+  description: 'מערכת לניהול נקודות וצבירת פרסים',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="he" dir="rtl">
